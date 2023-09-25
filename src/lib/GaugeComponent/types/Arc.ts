c8c302693d62ab9e51a107042b0b234151eef216
antoniolago <45375617+antoniolago@users.noreply.github.com>
2023-09-25 20:37:40 -0300

    Add tsdocs
    Fixes #16

--- src/lib/GaugeComponent/types/Arc.ts
+++ src/lib/GaugeComponent/types/Arc.ts
@@ -2,23 +2,39 @@ import { get } from 'lodash';
 import { GaugeType, defaultGaugeProps } from './GaugeComponentProps';
 import { Tooltip } from './Tooltip';
 export interface Arc {
-    cornerRadius?: number, //The corner radius of the arc
-    padding?: number, //The padding between subArcs, in rad
-    width?: number, //The width of the arc given in percent of the radius
-    nbSubArcs?: number, //The number of subArcs, this overrides "subArcs" limits
+    /** The corner radius of the arc. */
+    cornerRadius?: number,
+    /** The padding between subArcs, in rad. */
+    padding?: number, //
+    /** The width of the arc given in percent of the radius. */
+    width?: number, //
+    /** The number of subArcs, this overrides "subArcs" limits. */
+    nbSubArcs?: number, //
+    /** Boolean flag that enables or disables gradient mode, which
+     * draws a single arc with provided colors. */
     gradient?: boolean,
-    colorArray?: Array<string>, //The colors of the arcs, this overrides "subArcs" colors
+    /** The colors of the arcs, this overrides "subArcs" colors. */
+    colorArray?: Array<string>,
+    /** list of sub arcs segments of the whole arc. */
     subArcs?: Array<SubArc>
 }
 export interface SubArc {
-    limit?: number, //The limit of the subArc, in accord to the gauge value
-    color?: string | number, //The color of the subArc
-    length?: number, //The length of the subArc, in percent
+    /** The limit of the subArc, in accord to the gauge value. */
+    limit?: number,
+    /** The color of the subArc */
+    color?: string | number,
+    /** The length of the subArc, in percent */
+    length?: number,
     // needleColorWhenWithinLimit?: string, //The color of the needle when it is within the subArc
-    showTick?: boolean, //Whether or not to show the tick
+    /** Whether or not to show the tick */
+    showTick?: boolean,
+    /** Tooltip that appears onHover of the subArc */
     tooltip?: Tooltip,
+    /** This will trigger onClick of the subArc */
     onClick?: () => void,
+    /** This will trigger onMouseMove of the subArc */
     onMouseMove?: () => void,
+    /** This will trigger onMouseMove of the subArc */
     onMouseLeave?: () => void
 }
 export const defaultSubArcs: SubArc[] = [
--- src/lib/GaugeComponent/types/Labels.ts
+++ src/lib/GaugeComponent/types/Labels.ts
@@ -1,14 +1,21 @@
 import { defaultTickLabels, TickLabels } from './Tick';
 export interface Labels {
+    /** This configures the central value label. */
     valueLabel?: ValueLabel,
+    /** This configures the ticks and it's values labels. */
     tickLabels?: TickLabels
 }
 
 export interface ValueLabel {
+    /** This function enables to format the central value text as you wish. */
     formatTextValue?: (value: any) => string;
+    /** This will sync the value label color with the current value of the Gauge. */
     matchColorWithArc?: boolean;
+    /** This enables configuration for the number of decimal digits of the central value label */
     maxDecimalDigits?: number;
+    /** Central label value will inherit this */
     style?: React.CSSProperties;
+    /** This hides the central value label if true */
     hide?: boolean;
 }
 
